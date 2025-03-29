92f1dd6501d21f8891e416eacef1aaed3923a970
juanamador10 <127125858+juanamador1009@users.noreply.github.com>
2025-03-29 00:14:54 -0600

    Web3Forms POST request to handle contact form submission

--- main.js
+++ main.js
@@ -2,14 +2,59 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import fetch from "node-fetch";
+import multer from "multer";
 
 dotenv.config();
+
 const app = express();
+
+// Middleware para procesar datos JSON en el cuerpo de la solicitud
+// app.use(express.json());
+
+// Middleware para manejar datos de formularios con archivos adjuntos (multipart/form-data)
+const formParser = multer();
+
+// Middleware para permitir solicitudes desde otros orígenes (CORS)
 app.use(cors());
 
 // Routes
 app.get("/", (req, res) => {
-  res.json({ message: "nothing here to see" });
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Page not found</title>
+    </head>
+    <body>
+        <h1>My API Hub </h1>
+        <p>Different request to different APIs I use</p>
+    </body>
+    </html>
+  `);
+});
+
+// Web3Forms API Request - POST message (to my email)
+app.post("/contact/send", formParser.none(), async (req, res) => {
+  if (!req.body || Object.keys(req.body).length === 0) {
+    return res.status(400).json({ error: "No form data provided" });
+  }
+
+  const formData = { ...req.body, access_key: process.env.WEB3FORMS_API_KEY };
+
+  try {
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    });
+
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).send({ error: "Something went wrong on the server" });
+  }
 });
 
 // Open Weather Map API Request - GET Weather by City Name
